393d00075fb7c1dda7c5e223e952373ad39e2242
Jiayi <sjy.su@mail.utoronto.ca>
2021-06-16 08:08:32 -0400

    Sql commands to create the restaurants table

---
+++ db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE products (
+    id INT,
+    name VARCHAR(50),
+    price INT,
+    on_sale boolean
+);
+
+-- CREATE TABLE restaurants (
+--     id INT,
+--     name VARCHAR(50),
+--     location VARCHAR(50),
+--     price_range INT
+-- );
+
+CREATE TABLE restaurants (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL CHECK(price_range >= 1 and price_range <= 5)
+);
+
+INSERT INTO restaurants (id, name, location, price_range) 
+VALUES(123, 'mcdonalds', 'new yorks', 3);
+
