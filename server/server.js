198c4c8d336804dbbdf68782f3515d8eacae2ba6
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-06-16 13:28:59 -0400

    Use middleware

--- server/server.js
+++ server/server.js
@@ -3,13 +3,36 @@ const express = require("express");
 
 const app = express();
 
-app.get("/getRestaurants", (req, res) => {
-    // console.log("get all restaurants");
-    res.json({
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: "fail",
+    })
+})
+
+app.use((req, res, next) => {
+    console.log("This is our second middleware");
+    next();
+})
+
+app.get("/api/v1/restaurants", (req, res) => {
+    console.log("get all restaurants");
+    res.status(200).json({
         status: "success",
-        restaurant: "mcdonalds"
+        data: {
+            restaurant: ["mcdonalds", "wendys"]
+        }
     });
 });
+
+// get a restaurant
+app.get("/api/v1/restaurants/:restaurant_id", (req, res) => {
+    console.log(req.params);
+})
+
+// create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req);
+})
 
 
 const port = process.env.PORT || 3001;
