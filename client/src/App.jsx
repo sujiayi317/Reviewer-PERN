573a95f1d0c34fc762b57e8733a65906f47caec1
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-06-19 17:40:36 -0400

    Run npm i react-router-dom, create 3 pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import RestaurantDetailPage from './routes/RestaurantDetailPage'
+import UpdatePage from './routes/UpdatePage'
+import Home from './routes/Home'
 
 const App = () => {
-    return <div>App</div>
+    return <div>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/restaurants/:id/update" component={UpdatePage}></Route>
+                <Route exact path="/restaurants/:id" component={RestaurantDetailPage}></Route>
+            </Switch>
+        </Router>
+    </div>
 }
 
 export default App
