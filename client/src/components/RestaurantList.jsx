1751e63589f5cd5aeb843d590d7057fe73cb453f
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-06-20 18:30:11 -0400

    Rendering Restaurants

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -32,7 +32,19 @@ const RestaurantList = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="table-dark">
+                    {restaurants && restaurants.map(restaurant => {
+                        return (
+                            <tr key={restaurant.id}>
+                                <td>{restaurant.name}</td>
+                                <td>{restaurant.location}</td>
+                                <td>{"$".repeat(restaurant.price_range)}</td>
+                                <td>reviews</td>
+                                <td><button className="btn btn-warning">Update</button></td>
+                                <td><button className="btn btn-danger">Delete</button></td>
+                            </tr>
+                        )
+                    })}
+                    {/* <tr className="table-dark">
                         <td>mcdonalds</td>
                         <td>New York</td>
                         <td>$$</td>
@@ -47,7 +59,7 @@ const RestaurantList = (props) => {
                         <td>Rating</td>
                         <td><button className="btn btn-warning">Update</button></td>
                         <td><button className="btn btn-danger">Delete</button></td>
-                    </tr>
+                    </tr> */}
                 </tbody>
             </table>
             
