b2219410992d1592dd7f955b919deb73246b9f03
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-06-27 17:00:15 -0400

    Set up routing for update

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 import RestaurantFinder from '../apis/RestaurantFinder'
 import { RestaurantsContext } from '../context/RestaurantsContext'
 
 const RestaurantList = (props) => {
     const { restaurants, setRestaurants } = useContext(RestaurantsContext)
 
+    let history = useHistory()
     // ask the component to fetch the data as soon as it mounts to the screen
     useEffect(() => {
         const fetchData = async () => {
@@ -30,6 +32,10 @@ const RestaurantList = (props) => {
         }
     }
 
+    const handleUpdate = (id) => {
+        history.push(`/restaurants/${id}/update`)
+    }
+
     return (
         <div className="list-group">
             <table className="table table-hover">
@@ -51,7 +57,7 @@ const RestaurantList = (props) => {
                                 <td>{restaurant.location}</td>
                                 <td>{"$".repeat(restaurant.price_range)}</td>
                                 <td>reviews</td>
-                                <td><button className="btn btn-warning">Update</button></td>
+                                <td><button onClick={() => handleUpdate(restaurant.id)} className="btn btn-warning">Update</button></td>
                                 <td><button onClick={() => handleDelete(restaurant.id)} className="btn btn-danger">Delete</button></td>
                             </tr>
                         )
